bb5642d2107f8629d37465fbcf5d3a37ebb2c80d
Lem Apperson <lemapp09@gmail.com>
2023-09-30 19:35:01 -0400

    Added BattleShip, need to add to overall gameMaster

--- Assets/Scripts/TornPaper/BattleShipGame.cs
+++ Assets/Scripts/TornPaper/BattleShipGame.cs
@@ -7,12 +7,13 @@ namespace LemApperson.TornPaper
     public class BattleShipGame : MonoBehaviour
     {
         [SerializeField] private GameObject _buttonPrefab;
+        [SerializeField] private AudioClip[] _audioClips;
         private int[] _shipPositions;
+        private int _correctAnswers;
+        private bool _gameWon;
 
-        void Start()
-        {
-            for (int i = 0; i < 36; i++)
-            {
+        void Start() {
+            for (int i = 0; i < 36; i++) {
                 GameObject _button = Instantiate(_buttonPrefab, transform);
                 _button.transform.parent = transform;
                 _button.name = "Button" + i;
@@ -22,16 +23,30 @@ namespace LemApperson.TornPaper
         }
 
         public bool ShipHitted(int ButtonId) {
-            return CheckIfElementsMatch(ButtonId);
+            if (!_gameWon) {
+                return CheckIfElementsMatch(ButtonId);
+            }
+            return false;
         }
 
         bool CheckIfElementsMatch(int ButtonID) {
             for (int i = 0; i < _shipPositions.Length; i++) {
                 if (_shipPositions[i] == ButtonID) {
+                    _correctAnswers++;
+                    if (_correctAnswers == 10) {
+                        GameWon();
+                    }
+                    AudioManager.Instance.PlaySFX(_audioClips[0]);
                     return true;
                 }
             }
+            AudioManager.Instance.PlaySFX(_audioClips[1]);
             return false;
+        }
+
+        private void GameWon() {
+            // GameManager.Instance.GameWon();
+           _gameWon = true;
         }
     }
 }
