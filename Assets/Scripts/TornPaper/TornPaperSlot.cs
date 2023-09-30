998ad24bdc7eb13219a8a0eedf2ef3ca3971de66
Lem Apperson <lemapp09@gmail.com>
2023-09-30 11:53:29 -0400

    Added Torn Paper miniGame

---
+++ Assets/Scripts/TornPaper/TornPaperSlot.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+namespace LemApperson.TornPaper
+{
+    public class TornPaperSlot : MonoBehaviour,  IDropHandler
+    {
+        // Use USPS TornPaper Abbreviations
+        [SerializeField] public string _TornPaperName;
+        private Vector3 _initialPosition;
+        
+        
+        public void OnDrop(PointerEventData eventData)
+        {
+            String TornPaperName = eventData.pointerDrag.GetComponent<TornPaperPiece>()._TornPaperName;
+            if(TornPaperName == _TornPaperName) {
+                eventData.pointerDrag.transform.position = transform.position;
+                eventData.pointerDrag.transform.rotation = transform.rotation;
+                eventData.pointerDrag.transform.localScale = transform.localScale;
+                eventData.pointerDrag.GetComponent<TornPaperPiece>()._reachedTornPaperSlot = true;
+                //AudioManager.Instance.PlayClick();
+            }
+        }
+
+    }
+}
