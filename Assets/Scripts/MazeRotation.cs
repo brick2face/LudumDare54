9037b3302fce6515476f5b648f7b4e429a4e0fec
Ziros02 <32555668+Ziros02@users.noreply.github.com>
2023-10-02 18:34:33 -0400

    b

--- Assets/Scripts/MazeRotation.cs
+++ Assets/Scripts/MazeRotation.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MazeRotation : MonoBehaviour
 {
@@ -13,5 +14,9 @@ public class MazeRotation : MonoBehaviour
 
         
         transform.Rotate(Vector3.forward * horizontalInput * -rotationSpeed * Time.deltaTime);
+    }
+    public void reloadScene()
+    {
+        SceneManager.LoadScene("BallTestScene");
     }
 }
